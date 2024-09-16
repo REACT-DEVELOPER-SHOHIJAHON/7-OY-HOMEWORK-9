86953239229fa970f4fd958cc9d7b353da3984e5
REACT-DEVELOPER-SHOHIJAHON <shohijahongulomov32@gmail.com>
2024-09-16 17:05:50 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './redux/store/index.js';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './redux/store/index.js'
-import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
