5e03d5db031ad8555b9f2550ed700242cb5db375
REACT-DEVELOPER-SHOHIJAHON <shohijahongulomov32@gmail.com>
2024-09-16 17:17:33 +0500

    Update ProductsApi.js

--- src/redux/api/ProductsApi.js
+++ src/redux/api/ProductsApi.js
@@ -1,10 +1,10 @@
 import { api } from "./index";
 
 const productsApi = api.injectEndpoints({
-  endpoints: (build) => ({
-    getProducts: build.query({
-      query: () => ({
-        url: "/product/all",
+    endpoints: (build) => ({
+      getProducts: build.query({
+        query: () => ({
+          url: "/product/all",
       }),
     }),
   }),
