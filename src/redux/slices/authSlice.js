b674169b3f450e13ae32ad7f89d612bdc10f8c78
REACT-DEVELOPER-SHOHIJAHON <shohijahongulomov32@gmail.com>
2024-09-16 16:58:49 +0500

    Update authSlice.js

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  token: localStorage.getItem("token") || null,
+  token: localStorage.getItem("token"),
 };
 
 const authSlice = createSlice({
