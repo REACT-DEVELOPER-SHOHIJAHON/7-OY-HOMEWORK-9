1ad2874ab6aef6e3c9b8cddb59acd1946d008422
REACT-DEVELOPER-SHOHIJAHON <shohijahongulomov32@gmail.com>
2024-09-16 17:15:03 +0500

    Update authSlice.js

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,24 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const initialState = {
   token: localStorage.getItem("token"),
 };
 
+
 const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    signUp: (state, action) => {
-      state.token = action.payload.token;
-      localStorage.setItem("token", state.token);
-    },
-    login: (state, action) => {
-      state.token = action.payload.token;
-      localStorage.setItem("token", state.token);
-    },
-    logOut: (state) => {
-      state.token = null;
-      localStorage.removeItem("token");
+    name: "auth",
+    initialState,
+    reducers: {
+      signUp: (state, action) => {
+        state.token = action.payload.token;
+        localStorage.setItem("token", state.token);
+      },
+      login: (state, action) => {
+        state.token = action.payload.token;
+        localStorage.setItem("token", state.token);
+      },
+      logOut: (state) => {
+        state.token = null;
+        localStorage.removeItem("token");
     },
   },
 });
