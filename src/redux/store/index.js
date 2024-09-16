9ed0e7faafee4cc2db73ba13682f24404685e75d
REACT-DEVELOPER-SHOHIJAHON <shohijahongulomov32@gmail.com>
2024-09-16 17:13:49 +0500

    Update index.js

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { api } from "../api/index";
 import authReducer from "../slices/authSlice";
+import { api } from "../api/index";
+
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    [api.reducerPath]: api.reducer,
+      reducer: {
+      auth: authReducer,
+      [api.reducerPath]: api.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
+  
 });
